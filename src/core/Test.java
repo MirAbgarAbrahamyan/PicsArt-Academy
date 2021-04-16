8084a523aa5debf832ce20e7f37485e9c18dc55c
MirAbgarAbrahamyan <abgarabrahamyan1@gmail.com>
2021-04-16 21:09:07 +0400

    Test update

--- src/core/Test.java
+++ src/core/Test.java
@@ -1,7 +1,15 @@
 package core;
 
+import model.Human;
+import service.HumanService;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println(((int) (Math.random() * 9) + 1));
+        HumanService humanService = new HumanService();
+        Human[] humans = new Human[3];
+        for (int i = 0; i < 3; i++) {
+            humans[i] = humanService.create();
+        }
+        humanService.after2000(humans);
     }
 }
